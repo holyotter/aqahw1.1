385ee2e60577e46b97d48cfc798ecc8a1afa8f76
Vladislav Semenov <arctic.ratatat@gmail.com>
2022-01-13 23:12:25 +0300

    third commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,8 @@
 package ru.netology.service;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Assert;
+import org.junit.Test;
+
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
@@ -10,20 +11,20 @@ public class CashbackHackServiceTest {
     public void shouldReturn0() {
         int actual = service.remain(1000);
         int expected = 0;
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void shouldReturn100() {
         int actual = service.remain(900);
         int expected = 100;
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void shouldReturn500() {
         int actual = service.remain(500);
         int expected = 500;
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 }
